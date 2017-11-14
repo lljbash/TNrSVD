f5a81b03acfb3bfb949bc1e29a3d63f2a6ebc971
Li Lingjie <lljbash@gmail.com>
2017-11-14 10:35:07 +0800

    use ndSparse instead of tensor-toolbox

--- matrix2mpo.m
+++ matrix2mpo.m
@@ -45,13 +45,13 @@ for i=1:prod(n(2:end,2)) % i counts the columns
        R=R+length(rows{i});
    end
 end   
-
+return
 % initialize TN
 TN.core=cell(1,d);
 TN.n=[1 n(1,:) R;R*ones(d-2,1) n(2:end-1,:) R*ones(d-2,1);R n(end,:) 1];
-TN.core{1}=sptensor([prod(TN.n(1,1:end-1)),R]);
+TN.core{1}=ndSparse.build([prod(TN.n(1,1:end-1)),R]);
 for i=2:d
-    TN.core{i}=sptensor([TN.n(i,1),prod(TN.n(i,2:end-1)),TN.n(i,end)]);
+    TN.core{i}=ndSparse.build([TN.n(i,1),prod(TN.n(i,2:end-1)),TN.n(i,end)]);
 end
 
 r=1;
@@ -62,21 +62,19 @@ for i=1:prod(n(2:end,2))
            % matrices
            indices=[lin2ten(j,n(2:end,1))' lin2ten(i,n(2:end,2))'];
            temp=full(A((j-1)*tileX+1:j*tileX,(i-1)*tileY+1:i*tileY));
-           [ii,~,val]=find(temp(:));
-           TN.core{1}(:,r)=sptensor(ii,val);
+           TN.core{1}(:,r)=temp(:);
            for k=2:d-1
-               temp=sptensor(n(k,:));
+               temp=zeros(n(k,:));
                temp(indices(k-1,1),indices(k-1,2))=1;
-               TN.core{k}(r,:,r)=reshape(temp, prod(n(k,:)));
+               TN.core{k}(r,:,r)=temp(:);
            end
-           temp=sptensor(n(end,:));
+           temp=zeros(n(end,:));
            temp(indices(d-1,1),indices(d-1,2))=1;
-           TN.core{d}(r,:)=reshape(temp, prod(n(end,:)));
+           TN.core{d}(r,:)=temp(:)';
            r=r+1;
        end
    end
 end
-
 for i=1:size(TN.n,1)
     TN.core{i}=reshape(TN.core{i},TN.n(i,:));
 end
